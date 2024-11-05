ec604256b4698d665a9f4ce805ff91dc6a2ee514
João Paulo Thomaz Belphman <149329719+JoaoBalacobaco@users.noreply.github.com>
2024-11-05 12:23:53 -0300

    Delete orcamentoBD directory

--- orcamentoBD/src/main/java/br/com/orcamentobd/orcamentoBD/repositories/LancamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.orcamentobd.orcamentoBD.repositories;
-
-import br.com.orcamentobd.orcamentoBD.model.Lancamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LancamentoRepository extends JpaRepository<Lancamento,Long> {
-}
--- orcamentoBD/src/main/java/br/com/orcamentobd/orcamentoBD/repositories/MunicipioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.orcamentobd.orcamentoBD.repositories;
-
-import br.com.orcamentobd.orcamentoBD.model.Municipio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MunicipioRepository extends JpaRepository<Municipio,Integer> {
-}
